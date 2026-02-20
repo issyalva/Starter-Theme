7002f25db96199db8360fddf1b716d95bef87ef3
Isaac Alvarez <isaacalvarez@Isaacs-MacBook-Pro.local>
2026-02-20 15:51:04 -0500

    updates imports to extensionless

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@
 // Uses code splitting to load only components that exist on the page
 
 const componentMap = {
-  'ui-disclosure': () => import('./components/disclosure/disclosure.ts'),
+  'ui-disclosure': () => import('./components/disclosure/disclosure'),
   'ui-accordion-group': () =>
-    import('./components/accordion/accordion-group.ts'),
-  'ui-accordion-item': () => import('./components/accordion/accordion-item.ts'),
-  'dialog[is="ui-drawer"]': () => import('./components/dialog/drawer.js'),
-  'dialog[is="ui-modal"]': () => import('./components/dialog/modal.js'),
+    import('./components/accordion/accordion-group'),
+  'ui-accordion-item': () => import('./components/accordion/accordion-item'),
+  'dialog[is="ui-drawer"]': () => import('./components/dialog/drawer'),
+  'dialog[is="ui-modal"]': () => import('./components/dialog/modal'),
 };
 
 // Load components that exist on the page
